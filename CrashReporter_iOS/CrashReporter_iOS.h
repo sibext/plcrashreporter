17766b23daf62b3a31766c19c1443564f58d18bb
Artyom Gladkov <gladkov@sibext.com>
2015-11-26 14:07:12 +0600

    Add new build target (ios Framework)

---
+++ CrashReporter_iOS/CrashReporter_iOS.h
@@ -0,0 +1,19 @@
+//
+//  CrashReporter_iOS.h
+//  CrashReporter_iOS
+//
+//  Created by Artem Gladkov on 26.11.15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CrashReporter_iOS.
+FOUNDATION_EXPORT double CrashReporter_iOSVersionNumber;
+
+//! Project version string for CrashReporter_iOS.
+FOUNDATION_EXPORT const unsigned char CrashReporter_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CrashReporter_iOS/PublicHeader.h>
+
+
