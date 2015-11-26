be8effd3d82b30a9b35555a8528ebaf0b3cac6b7
Artyom Gladkov <gladkov@sibext.com>
2015-11-26 15:35:53 +0600

    Modify umbrella, fix product paths

--- CrashReporter_iOS/CrashReporter_iOS.h
+++ CrashReporter_iOS/CrashReporter_iOS.h
@@ -16,4 +16,26 @@ FOUNDATION_EXPORT const unsigned char CrashReporter_iOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CrashReporter_iOS/PublicHeader.h>
 
-
+#import <CrashReporter_iOS/CrashReporter.h>
+#import <CrashReporter_iOS/PLCrashNamespace.h>
+#import <CrashReporter_iOS/PLCrashReport.h>
+#import <CrashReporter_iOS/PLCrashReporter.h>
+#import <CrashReporter_iOS/PLCrashReportApplicationInfo.h>
+#import <CrashReporter_iOS/PLCrashReportBinaryImageInfo.h>
+#import <CrashReporter_iOS/PLCrashReportRegisterInfo.h>
+#import <CrashReporter_iOS/PLCrashReportStackFrameInfo.h>
+#import <CrashReporter_iOS/PLCrashReportExceptionInfo.h>
+#import <CrashReporter_iOS/PLCrashReportThreadInfo.h>
+#import <CrashReporter_iOS/PLCrashReportSystemInfo.h>
+#import <CrashReporter_iOS/PLCrashAsyncSignalInfo.h>
+#import <CrashReporter_iOS/PLCrashReportSymbolInfo.h>
+#import <CrashReporter_iOS/PLCrashReportProcessInfo.h>
+#import <CrashReporter_iOS/PLCrashReportSignalInfo.h>
+#import <CrashReporter_iOS/PLCrashFeatureConfig.h>
+#import <CrashReporter_iOS/PLCrashReporterConfig.h>
+#import <CrashReporter_iOS/PLCrashReportMachExceptionInfo.h>
+#import <CrashReporter_iOS/PLCrashMacros.h>
+#import <CrashReporter_iOS/PLCrashReportTextFormatter.h>
+#import <CrashReporter_iOS/PLCrashReportFormatter.h>
+#import <CrashReporter_iOS/PLCrashReportMachineInfo.h>
+#import <CrashReporter_iOS/PLCrashReportProcessorInfo.h>
